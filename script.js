3ca1a9e7e703d49e75b0adc91103e5368fd222b1
Ian A. Cook <6866411+nai888@users.noreply.github.com>
2019-02-07 20:33:49 -0600

    Add navigation

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
 // Spinner data
-var spinnerData = {
-  spinners: [
-    {
-      classes: 'cls-circle cls-spin'
-    },
-    {
-      classes: 'cls-dual-circle cls-spin'
-    },
-    {
-      classes: 'cls-square cls-flip-flop'
-    },
-    {
-      classes: 'cls-bowtie cls-spin'
-    },
-    {
-      classes: 'cls-bowtie cls-flip-flop'
-    },
-    {
-      classes: 'cls-signal'
-    },
-    {
-      classes: 'cls-triple-signal'
-    },
-    {
-      classes: 'cls-ring'
-    },
-    {
-      classes: 'cls-rings'
-    }
-  ]
-}
-
+var spinnerData = [
+  {
+    classes: 'cls-circle cls-spin'
+  },
+  {
+    classes: 'cls-dual-circle cls-spin'
+  },
+  {
+    classes: 'cls-square cls-flip-flop'
+  },
+  {
+    classes: 'cls-bowtie cls-spin'
+  },
+  {
+    classes: 'cls-bowtie cls-flip-flop'
+  },
+  {
+    classes: 'cls-signal'
+  },
+  {
+    classes: 'cls-triple-signal'
+  },
+  {
+    classes: 'cls-ring'
+  },
+  {
+    classes: 'cls-rings'
+  }
+]
 var currentSpinner = 0
 
 // Determine the correct copyright years to list
@@ -46,6 +43,42 @@ var getCopyrightYears = function () {
 }
 
 window.onload = function () {
+  // Fix the copyright year if needed
   var copyrightYears = document.getElementById('copyright-years')
   copyrightYears.innerHTML = getCopyrightYears()
+
+  // Animate the arrow styling
+  var navArrows = Array.from(document.getElementsByClassName('nav-arrow'))
+  navArrows.forEach(function (arr) {
+    arr.onmouseover = function (e) {
+      e.target.classList.add('hovered')
+    }
+    arr.onmouseout = function (e) {
+      e.target.classList.remove('hovered')
+    }
+  })
+
+  // Change the spinner and instructions text
+  var spinner = document.querySelector('.cls-spinner').children[0]
+  console.log(spinner)
+  var instructionsClasses = document.getElementById('classes')
+  var setSpinnerData = function () {
+    var newClasses = spinnerData[currentSpinner].classes
+    spinner.className = newClasses
+    instructionsClasses.innerText = newClasses
+  }
+
+  // Navigate to previous
+  var prevArrow = document.getElementById('previous')
+  prevArrow.onclick = function () {
+    currentSpinner = (currentSpinner + spinnerData.length - 1) % spinnerData.length
+    setSpinnerData()
+  }
+
+  // Navigate to next
+  var prevArrow = document.getElementById('next')
+  prevArrow.onclick = function () {
+    currentSpinner = (currentSpinner + 1) % spinnerData.length
+    setSpinnerData()
+  }
 }
