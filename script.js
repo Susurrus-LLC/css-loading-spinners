5209a15a25becea7ee2817375ba1e5f389cf06c9
Ian A. Cook <6866411+nai888@users.noreply.github.com>
2019-02-07 19:03:01 -0600

    Add spinners data object

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+// Spinner data
+var spinnerData = {
+  spinners: [
+    {
+      classes: 'cls-circle cls-spin'
+    },
+    {
+      classes: 'cls-dual-circle cls-spin'
+    },
+    {
+      classes: 'cls-square cls-flip-flop'
+    },
+    {
+      classes: 'cls-bowtie cls-spin'
+    },
+    {
+      classes: 'cls-bowtie cls-flip-flop'
+    },
+    {
+      classes: 'cls-signal'
+    },
+    {
+      classes: 'cls-triple-signal'
+    },
+    {
+      classes: 'cls-ring'
+    },
+    {
+      classes: 'cls-rings'
+    }
+  ]
+}
+
+var currentSpinner = 0
+
+// Determine the correct copyright years to list
 var getCopyrightYears = function () {
   var startYear = 2019
   var date = new Date
